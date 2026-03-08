f74b2b703910562b0ed2923647ef09f3b257f02b
joemalynsalahog <salahogjoemalynjoy@gmail.com>
2026-03-08 09:40:23 +0800

    Ideas and Impact Animation

---
+++ script.js
@@ -0,0 +1,15 @@
+
+window.addEventListener("load", () => {
+    const ideas = document.getElementById("ideas");
+    const impact = document.getElementById("impact");
+
+    setTimeout(() => {
+        ideas.style.opacity = "1";
+        ideas.style.transform = "translateY(0)";
+    }, 300);
+
+    setTimeout(() => {
+        impact.style.opacity = "1";
+        impact.style.transform = "translateY(0)";
+    }, 800);
+});
